af5ce347e85675eb92e90a1d14fb33f7e5df2fb0
sachin777s <stdtech7878@gmail.com>
2024-01-01 10:31:07 +0530

    Search banner is created

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,10 +1,10 @@
 "use client";
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from "./favicon.ico"
 import { MdKeyboardArrowDown } from "react-icons/md";
 import Link from 'next/link';
-import { BsPersonCircle } from "react-icons/bs";
+import { BsPersonCircle, BsWindowDesktop } from "react-icons/bs";
 
 export default function Navbar() {
     const [isLogin, setIsLogin] = useState<boolean>(true);
@@ -46,8 +46,25 @@ export default function Navbar() {
             path: "/"
         }
     ]
+
+    //For navbar scrolling behavior
+    const [isNavCollored, setIsNavCollored] = useState<boolean>(false);
+    useEffect(() => {
+        const controllNavbar = () => {
+            if (window.scrollY > 150) {
+                setIsNavCollored(true)
+            } else {
+                setIsNavCollored(false);
+            }
+        }
+        window.addEventListener("scroll", controllNavbar)
+        return () => {
+            window.removeEventListener("scroll", controllNavbar)
+        }
+    }, [])
+
     return (
-        <nav className='w-full h-[65px] px-2 md:px-4 flex items-center justify-between'>
+        <nav className={`w-full h-[65px] px-2 md:px-4 transition-all duration-500 ${isNavCollored && `bg-[var(--bg-primary)]`} flex items-center justify-between fixed z-50 `}>
             <div className='flex gap-2 items-center'>
                 <Image className='w-8 h-8 md:h-10 md:w-10' height={40} width={40} src={Logo} alt='...' />
                 <span className='hidden sm:block text-xl md:text-2xl'>Image-Influx</span>
@@ -55,10 +72,10 @@ export default function Navbar() {
             <div className='flex items-center gap-2 md:gap-4'>
                 <button className='relative group'>
                     <span className='flex items-center'>Explore <span className=" transition-transform duration-150 group-hover:rotate-180"><MdKeyboardArrowDown size={20} /></span> </span>
-                    <ul className='py-1 md:py-2 bg-[var(--secondary-color)] absolute top-full left-0 hidden group-hover:flex hover:flex flex-col rounded-md border border-[var(--secondary-color)] shadow-[0_0_12px_-3px_var(--secondary-color)]'>
+                    <ul className='py-1 md:py-2 bg-[var(--bg-primary)] absolute top-full right-0 hidden group-hover:flex hover:flex flex-col rounded-md border border-[var(--secondary-color)] shadow-[0_0_12px_-3px_var(--secondary-color)]'>
                         {
-                            exploreRoutes.map((route) => {
-                                return <li><Link className='px-4 py-1' href={route.path}>{route.name}</Link></li>
+                            exploreRoutes.map((route, index) => {
+                                return <li key={index}><Link className='px-4 py-1 hover:opacity-[70%]' href={route.path}>{route.name}</Link></li>
                             })
                         }
                     </ul>
@@ -66,10 +83,10 @@ export default function Navbar() {
                 {
                     isLogin && <div className='relative group'>
                         <span className='flex items-center'><BsPersonCircle size={30} /><MdKeyboardArrowDown className="transition-transform duration-150 group-hover:rotate-180" size={20} /></span>
-                        <ul className='py-1 md:py-2 bg-[var(--secondary-color)] absolute right-0 rounded-md border border-[var(--secondary-color)] hidden group-hover:block'>
+                        <ul className='py-1 md:py-2 bg-[var(--bg-primary)] absolute right-0 rounded-md border border-[var(--secondary-color)] hidden group-hover:block'>
                             {
-                                profileRoutes.map((route) => {
-                                    return <li><Link className='px-4 py-1' href={route.path}>{route.name}</Link></li>
+                                profileRoutes.map((route, index) => {
+                                    return <li key={index}><Link className='px-4 py-1 hover:opacity-[70%]' href={route.path}>{route.name}</Link></li>
                                 })
                             }
                         </ul>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+"use client";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { IoMdSearch } from "react-icons/io";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [isVideo, setIsVideo] = useState<boolean>(false)
+  const [query, setQuery] = useState<string>("");
+
   return (
     <main>
-     
+      <div className="section-div w-full h-[500px] bg-[url('https://images.pexels.com/photos/17279373/pexels-photo-17279373/free-photo-of-istanbul-bahcekoy.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load')] bg-cover bg-center flex flex-col items-center justify-center gap-2 md:gap-4">
+        <h1 className="max-w-[45rem] text-3xl font-bold">The best free stock photos, royalty free images & videos shared by creators.</h1>
+        <div className="w-full md:w-[700px] h-[45px] px-2 bg-[var(--bg-primary)] rounded-full flex items-center justify-between">
+          <IoMdSearch size={20} />
+          <input
+            className="w-[calc(100%-110px)] border-none outline-none bg-transparent"
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <div className="relative group">
+            <span className="flex items-center">{isVideo ? "Videos" : "Images"}<MdKeyboardArrowDown /></span>
+            <ul className="absolute -left-2 bg-[var(--bg-primary)] border-2 border-[var(--secondary-color)] rounded-md hidden group-hover:block">
+              <li><span className="flex items-center my-1 mx-2 cursor-default" onClick={() => setIsVideo(false)}>Images</span></li>
+              <li><span className="flex items-center my-1 mx-2 cursor-default" onClick={() => setIsVideo(true)}>Videos</span></li>
+            </ul>
+          </div>
+        </div>
+        <p className="text-base">"Capturing Moments, Creating Memories: Your Gateway to a Visual Symphony!"</p>
+      </div>
     </main>
   )
 }
